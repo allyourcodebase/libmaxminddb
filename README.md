74861e10ef14592038cecbb0434d4a5f5a28466d
Jora Troosh <tensorush@gmail.com>
2024-07-19 12:57:58 +0300

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# libmaxminddb
+
+[![CI][ci-shd]][ci-url]
+[![LC][lc-shd]][lc-url]
+
+## Zig build of [libmaxminddb library](https://github.com/maxmind/libmaxminddb).
+
+### :rocket: Usage
+
+- Add `libmaxminddb` dependency to `build.zig.zon`.
+
+```sh
+zig fetch --save https://github.com/allyourcodebase/libmaxminddb/archive/<git_tag_or_commit_hash>.tar.gz
+```
+
+- Use `libmaxminddb` dependency in `build.zig`.
+
+```zig
+const libmaxminddb_dep = b.dependency("libmaxminddb", .{
+    .target = target,
+    .optimize = optimize,
+});
+const mmdb_mod = libmaxminddb_dep.module("mmdb");
+<compile>.root_module.addImport("mmdb", mmdb_mod);
+```
+
+<!-- MARKDOWN LINKS -->
+
+[ci-shd]: https://img.shields.io/github/actions/workflow/status/allyourcodebase/libmaxminddb/ci.yaml?branch=main&style=for-the-badge&logo=github&label=CI&labelColor=black
+[ci-url]: https://github.com/allyourcodebase/libmaxminddb/blob/main/.github/workflows/ci.yaml
+[lc-shd]: https://img.shields.io/github/license/allyourcodebase/libmaxminddb.svg?style=for-the-badge&labelColor=black
+[lc-url]: https://github.com/allyourcodebase/libmaxminddb/blob/main/LICENSE
---
+++ build.zig
@@ -0,0 +1,82 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const root_source_file = b.path("src/lib.zig");
+    const version = std.SemanticVersion{ .major = 1, .minor = 10, .patch = 0 };
+
+    // Dependencies
+    const libmaxminddb_dep = b.dependency("libmaxminddb", .{});
+    const libmaxminddb_src_path = libmaxminddb_dep.path("src/");
+    const libmaxminddb_inc_path = libmaxminddb_dep.path("include/");
+
+    // Library
+    const lib_step = b.step("lib", "Install library");
+
+    const lib = b.addStaticLibrary(.{
+        .name = "mmdb",
+        .target = target,
+        .version = version,
+        .optimize = optimize,
+    });
+    lib.root_module.addCMacro("PACKAGE_VERSION", b.fmt("\"{}\"", .{version}));
+    lib.addCSourceFiles(.{ .root = libmaxminddb_src_path, .files = &SOURCES, .flags = &FLAGS });
+    lib.installHeader(libmaxminddb_inc_path.path(b, "maxminddb.h"), "");
+    lib.addConfigHeader(b.addConfigHeader(.{
+        .style = .{ .cmake = libmaxminddb_inc_path.path(b, "maxminddb_config.h.cmake.in") },
+        .include_path = "maxminddb_config.h",
+    }, VALUES));
+    lib.addIncludePath(libmaxminddb_inc_path);
+    lib.linkLibC();
+
+    if (target.result.cpu.arch.endian() == .little) {
+        lib.root_module.addCMacro("MMDB_LITTLE_ENDIAN", "1");
+    }
+
+    const lib_install = b.addInstallArtifact(lib, .{});
+    lib_step.dependOn(&lib_install.step);
+    b.default_step.dependOn(lib_step);
+
+    // Bindings module
+    const mod = b.addModule("mmdb", .{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = root_source_file,
+    });
+    mod.linkLibrary(lib);
+
+    // Formatting checks
+    const fmt_step = b.step("fmt", "Run formatting checks");
+
+    const fmt = b.addFmt(.{
+        .paths = &.{
+            "src/",
+            "build.zig",
+        },
+        .check = true,
+    });
+    fmt_step.dependOn(&fmt.step);
+    b.default_step.dependOn(fmt_step);
+}
+
+const HEADERS = .{
+    "data-pool.h",
+    "maxminddb-compat-util.h",
+};
+
+const SOURCES = .{
+    "data-pool.c",
+    "maxminddb.c",
+};
+
+const FLAGS = .{
+    "-std=c99",
+    "-O2",
+    "-g",
+};
+
+const VALUES = .{
+    .MMDB_UINT128_USING_MODE = 0,
+    .MMDB_UINT128_IS_BYTE_ARRAY = if (@sizeOf(u128) == 16) 0 else 1,
+};
